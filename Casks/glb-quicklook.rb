44ff9e91a35ec866e2ec210a251f22d8f33c26c8
trapple <trapplejp@gmail.com>
2026-07-12 19:00:03 +0900

    glb-quicklook 1.0.0

--- Casks/glb-quicklook.rb
+++ Casks/glb-quicklook.rb
@@ -1,6 +1,6 @@
 cask "glb-quicklook" do
-  version "0.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  version "1.0.0"
+  sha256 "bcd8d4a6732768084bb9f204812ec4948ee63f7255c550a1b3e3c01705cb1d6d"
 
   url "https://github.com/trapple/glb-quicklook/releases/download/v#{version}/GLBQuickLook-#{version}.zip"
   name "GLB Quick Look"
